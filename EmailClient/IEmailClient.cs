770ea7d680b16331ea37c7c64c88bc4b71e9cb61
David <lejonmanen@gmail.com>
2017-02-01 10:16:10 +0100

    initial commit

---
+++ EmailClient/IEmailClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient
+{
+    interface IEmailClient
+    {
+        void Setup(string myEmail, string myName);
+
+        void SendEmail(string to, string header, string body);
+    }
+}
---
+++ EmailClient/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient
+{
+    interface IEmailService
+    {
+        void SendEmail(string to, string from,
+            string header, string body);
+    }
+}
